80ad81e67011da959187c88a9aa6ba6503c69e53
NIX\bence.balogh <bence.balogh@nixs.com>
2025-07-10 10:27:56 +0200

    fail test to test pipeline test

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -5,6 +5,7 @@ test('has title', async ({ page }) => {
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Playwright/);
+  await expect(false).toBeTruthy();
 });
 
 test('get started link', async ({ page }) => {
