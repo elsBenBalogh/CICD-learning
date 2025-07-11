585125537d959d7a3cb4f56a17afda294127e496
NIX\bence.balogh <bence.balogh@nixs.com>
2025-07-11 10:50:13 +0200

    webserver startup

--- globalSetup.ts
+++ globalSetup.ts
@@ -1,7 +1,7 @@
 import * as dotenv from "dotenv";
 
 async function globalSetup() {
-    debugger;
+
     process.env.ENV = process.env.ENV || "local";
     dotenv.config({
         path: `./envs/.env.${process.env.ENV}`,
--- playwright.config.ts
+++ playwright.config.ts
@@ -12,6 +12,11 @@ import { defineConfig, devices } from '@playwright/test';
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
+  webServer: {
+    command: 'npx serve -s dist -l 4173',
+    port: 4173,
+    timeout: 30000
+  },
   testDir: './tests',
   /* Run tests in files in parallel */
   fullyParallel: true,
